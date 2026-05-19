625053b1fb063a0c94a7150865745536ef77d95a
LEANDRA CRUZ DE LEMOS <04785634006@senacrs.edu.br>
2026-05-19 19:21:02 -0300

    adicionei as aulas

---
+++ meuprojeto2/src/meuprojeto2/Meuprojeto2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package meuprojeto2;
+
+/**
+ *
+ * @author LEANDRACRUZDELEMOS
+ */
+public class Meuprojeto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Meu nome e Leandra Lemos");
+        System.out.println("Minha idade e 19")  ;
+        System.out.println("Nao tenho time sor, prefiro ler");
+        System.out.println("Moro em Sao Leopoldo");
+    }
+    
+}
