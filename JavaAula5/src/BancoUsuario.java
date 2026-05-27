4206bd5980c904701136d71052aa1ebc9894e477
LEANDRA CRUZ DE LEMOS <04785634006@senacrs.edu.br>
2026-05-26 21:12:20 -0300

    Aula 5 adicionada

---
+++ JavaAula5/src/BancoUsuario.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.ArrayList;
+/**
+ *
+ * @author LEANDRACRUZDELEMOS
+ */
+ public class BancoUsuario {
+    private static ArrayList<Usuario> usuarios = new ArrayList<>();
+    
+    public static void adicionarUsuario(Usuario usuario){
+    usuarios.add(usuario);
+    }
+
+
+  public static boolean ValidarLogin(String email, String senha){
+    for (Usuario u : usuarios) {
+    if(u.getEmail().equals(email) && u.getSenha().equals(senha)){
+        return true;
+    }
+    }
+    return false;
+    }
+    
+    public static String ValidarNome(String email, String senha){
+        for (Usuario u : usuarios){
+            if (u.getEmail().equals(email) && u.getSenha().equals(senha)){
+                return u.getNome();
+            }
+        }
+        return "Sem nome";
+    }
+ }
